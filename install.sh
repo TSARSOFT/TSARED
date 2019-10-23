d5c4b5c898ebf7d022cffb40c0a255fe58d965f4
TSARSOFT <tsarsoftdev@gmail.com>
2019-10-23 14:55:00 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir ~/TSARSOFT
 mkdir ~/TSARSOFT/TSARED
 cd ~/TSARSOFT/TSARED
-rm ~/TSARSOFT/TSARED/*
+rm ~/TSARSOFT/TSARED/* -r
 wget 'https://github.com/TSARSOFT/TSARED/blob/master/TSARED-latest.zip?raw=true'
 mv ~/TSARSOFT/TSARED/TSARED-latest.zip\?raw=true ~/TSARSOFT/TSARED/TSARED-latest.zip
 unzip ./TSARED-latest.zip
